cc717f8117a3b755c09d789c8be3ec157ed4d4be
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-08-03 18:20:23 +0900

    a

--- main.cpp
+++ main.cpp
@@ -1425,8 +1425,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 #endif
 	
 	
-	//出力ウィンドウへの文字出力　実行すると出る下の文字
-	OutputDebugStringA("Hello,DirectX!\n");
+	////出力ウィンドウへの文字出力　実行すると出る下の文字
+	//OutputDebugStringA("Hello,DirectX!\n");
 
 	return 0;
 }
