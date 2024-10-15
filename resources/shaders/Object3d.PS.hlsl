6288d5a8cbdedbef01448744e1ef7ffebe94c6fc
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-10-15 14:11:59 +0900

    fenceの重なりが正常になるように修正

--- resources/shaders/Object3d.PS.hlsl
+++ resources/shaders/Object3d.PS.hlsl
@@ -47,25 +47,29 @@ PixelShaderOutput main(VertexShaderOutput input)
             float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
             float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
             output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
-        if (textureColor.a <= 0.5)
-        {
-            discard;
-        }
-        if (textureColor.a == 0.0)
-        {
-            discard;
-        }
-       
-        if (output.color.a == 0.0)
-        {
-            discard;
-        }
+        
+            
         }
        
            
     }else{
         output.color = gMaterial.color * textureColor;
     }
+    if (textureColor.a <= 0.5)
+    {
+        discard;
+    }
+    if (textureColor.a == 0.0)
+    {
+        discard;
+    }
+       
+    if (output.color.a == 0.0)
+    {
+        discard;
+    }
+            
     return output;
+    
 }
 
