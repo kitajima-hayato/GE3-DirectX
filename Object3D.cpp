b95c887fca0b5d4701847c15c6307cd140d95168
kitajima-hayato <k023g0057@g.neec.ac.jp>
2024-11-25 12:38:55 +0900

    ６章完了/ブランチ切るの忘れた
    プロジェクトにモデル管理機能を追加
    
    `CG2_DirectX.vcxproj` に `Model.cpp`、`ModelCommon.cpp`、`ModelManager.cpp` のソースファイルと `Model.h`、`ModelCommon.h`、`ModelManager.h` のヘッダーファイルを追加しました。
    `CG2_DirectX.vcxproj.filters` にも同様に追加しました。
    `DirectXCommon.cpp` の `CreateBufferResource` メソッドで、`CreateCommittedResource` の呼び出しを改行して見やすくしました。
    `Object3D.cpp` に `ModelManager.h` をインクルードし、`Object3D` クラスの初期化メソッドでモデルデータの読み込みとテクスチャの設定を削除しました。また、`SetModel` メソッドを追加し、モデルの描画処理を簡素化しました。
    `Object3D.h` に `Model` クラスのインクルードと、モデルの設定用のメソッドを追加しました。また、不要なメソッドとメンバ変数を削除しました。
    `Object3DCommon.h` のプライベートメンバ変数の位置を変更しました。
    `main.cpp` に `Model.h`、`ModelCommon.h`、`ModelManager.h` をインクルードし、`WinMain` 関数内で `ModelManager` の初期化と終了処理を追加しました。また、`Object3D` インスタンスにモデルを設定する処理を追加し、複数のモデルと `Object3D` インスタンスを作成するようにしました。
    `main.cpp` の `WinMain` 関数内で、`object3D2` の更新と描画処理を追加しました。
    `main.cpp` の終了処理で、`model2`、`object3D2`、`model` の削除を追加しました。
    `Model.cpp` に `#include "Model.h"` と `using namespace std;` を追加し、`Model` クラスに `Initialize`、`Draw`、`LoadObjFile`、`LoadMaterialTempLateFile`、`CreateVertexResourceData`、`CreateMaterialResource` メソッドを追加しました。
    `Model.h` に `#pragma once` と必要なヘッダーファイルのインクルードを追加し、`Model` クラスを定義しました。
    `ModelCommon.cpp` に `#include "ModelCommon.h"` を追加し、`ModelCommon` クラスのコンストラクタと `Initialize` メソッドを追加しました。
    `ModelCommon.h` に `#pragma once` と必要なヘッダーファイルのインクルードを追加し、`ModelCommon` クラスを定義しました。
    `ModelManager.cpp` に `#include "ModelManager.h"` を追加し、`ModelManager` クラスのシングルトンインスタンスを管理するためのメンバ変数 `instance` を追加しました。
    `ModelManager.h` に `#pragma once` と必要なヘッダーファイルのインクルードを追加し、`ModelManager` クラスを定義しました。

--- Object3D.cpp
+++ Object3D.cpp
@@ -4,19 +4,15 @@
 #include <sstream>
 #include <cassert>
 #include "MakeMatrix.h"
+#include "ModelManager.h"
 using namespace std;
 void Object3D::Initialize(Object3DCommon* obj3dCommon)
 {
 	this->object3DCommon = obj3dCommon;
-	modelData = LoadObjFile("resources", "plane.obj");
-	CreateVertexResourceData();
-	CreateMaterialResource();
+	
+
 	CreateTransformationMatrixData();
 	CreateDirectionalLightResource();
-	// .objの参照しているテクスチャファイル読み込み
-	TextureManager::GetInstance()->LoadTexture(modelData.material.textureFilePath);
-	// 読み込んだテクスチャのインデックスを取得
-	TextureManager::GetInstance()->GetTextureIndexByFilePath(modelData.material.textureFilePath);
 
 	// Transformの初期化
 	transform={ { 1.0f, 1.0f, 1.0f },{ 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f  } };
@@ -32,155 +28,28 @@ void Object3D::Update()
 	Matrix4x4 worldViewProjection = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
 	wvpData->WVP = worldViewProjection;
 	wvpData->World = worldMatrix;
-
-
 }
 
 void Object3D::Draw()
-{
-	// 頂点バッファービューをセット
-	object3DCommon->GetDxCommon()->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
-	// マテリアルデータをセット
-	object3DCommon->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
-	// 座標変換行列をセット
+{// 座標変換行列をセット
 	object3DCommon->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource->GetGPUVirtualAddress());
-	object3DCommon->GetDxCommon()->GetCommandList()->SetGraphicsRootDescriptorTable(2, TextureManager::GetInstance()->GetSrvHandleGPU(modelData.material.textureIndex) );
 	// 平行光源データをセット
 	object3DCommon->GetDxCommon()->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource->GetGPUVirtualAddress());
-	// テクスチャをセット
-	//object3DCommon->GetDxCommon()->GetCommandList()->SetGraphicsRoot32BitConstant(3, modelData.material.textureIndex, 0);
-	// 描画
-	object3DCommon->GetDxCommon()->GetCommandList()->DrawInstanced(UINT(modelData.vertices.size()), 1, 0, 0);
-}
-
-MaterialData Object3D::LoadMaterialTempLateFile(const std::string& directoryPath, const std::string& filename)
-{
-	MaterialData materialData;	//構築するMaterialData
-	std::string line;			//ファイルから読んだ１行を格納するもの
-	std::ifstream file(directoryPath + "/" + filename);//ファイルを開く
-	assert(file.is_open());		//開けなかったら止める
-
-	while (std::getline(file, line)) {
-		std::string identifier;
-		std::istringstream s(line);
-		s >> identifier;
-		//identifierに応じた処理
-		if (identifier == "map_Kd") {
-			std::string textureFilename;
-			s >> textureFilename;
-			//連結してファイルパスにする
-			materialData.textureFilePath = directoryPath + "/" + textureFilename;
-		}
+	// 3Dモデルが割り当てられていれば描画する
+	if (model) {
+		model->Draw();
 	}
-	return materialData;
 }
 
-ModelData Object3D::LoadObjFile(const std::string& directoryPath, const std::string& filename)
+void Object3D::SetModel(const std::string& filePath)
 {
-	// 1. 必要な変数宣言
-	ModelData modelData;     // 構築するModelData
-	vector<Vector4> positions;    // 位置
-	vector<Vector3> normals;    // 法線
-	vector<Vector2> texcoords;    // テクスチャ座標
-	string line;       // ファイルから読んだ1行を格納
-
-	// 2. ファイルを開く
-	ifstream file(directoryPath + "/" + filename);
-	assert(file.is_open());         // 開けなかったら止める
-
-	while (getline(file, line)) {
-		string identifier;
-		istringstream s(line);
-		s >> identifier; // 先頭の識別子を読む
-
-		// identifierに応じた処理
-		if (identifier == "v") {
-			Vector4 position;
-			s >> position.x >> position.y >> position.z;
-			position.x *= -1;
-			position.w = 1.0f;
-			positions.push_back(position);
-		}
-		else if (identifier == "vt") {
-			Vector2 texcoord;
-			s >> texcoord.x >> texcoord.y;
-			texcoord.y = 1.0f - texcoord.y;
-			texcoords.push_back(texcoord);
-		}
-		else if (identifier == "vn") {
-			Vector3 normal;
-			s >> normal.x >> normal.y >> normal.z;
-			normal.x *= -1;
-			normals.push_back(normal);
-		}
-		else if (identifier == "f") {
-			VertexData triangle[3];
-			// 面は三角形限定。その他は未定。
-			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
-				string vertexDefinition;
-				s >> vertexDefinition;
-				// 頂点の要素へのIndexは　［位置/UV/法線］　で格納されているので分解してIndexを取得する
-				istringstream v(vertexDefinition);
-				uint32_t elementIndices[3];
-				for (int32_t element = 0; element < 3; ++element) {
-					string index;
-					getline(v, index, '/');      // 区切りでインデックスうぃ読んでいく
-					elementIndices[element] = stoi(index);
-				}
-				// 要素へのIndexから、実際の要素の値を取得して、頂点を構築する
-				Vector4 position = positions[elementIndices[0] - 1];
-				Vector2 texcoord = texcoords[elementIndices[1] - 1];
-				Vector3 normal = normals[elementIndices[2] - 1];
-
-				triangle[faceVertex] = { position,texcoord,normal };
-			}
-			// 頂点を逆順すろことで、回り順を逆にする
-			modelData.vertices.push_back(triangle[2]);
-			modelData.vertices.push_back(triangle[1]);
-			modelData.vertices.push_back(triangle[0]);
-		}
-		else if (identifier == "mtllib") {
-			std::string materialFilename;
-			s >> materialFilename;
-			modelData.material = LoadMaterialTempLateFile(directoryPath, materialFilename);
-		}
-	}
-	return modelData;
+	// モデルを検索してセットする
+	model = ModelManager::GetInstance()->FindModel(filePath);
 }
 
-void Object3D::CreateVertexResourceData()
-{
-	// 頂点バッファーリソースを作成
-	vertexResource = object3DCommon->GetDxCommon()->CreateBufferResource(sizeof(VertexData) * modelData.vertices.size());
-	// 頂点バッファーリソースにデータを書き込む
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	std::memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
-	// 頂点バッファービューを作成
-	// リソースの先頭のアドレスから使用
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	// 使用するリソースのサイズは頂点のサイズ
-	vertexBufferView.SizeInBytes = UINT(sizeof(VertexData) * modelData.vertices.size());
-	vertexBufferView.StrideInBytes = sizeof(VertexData);
-	//書き込むためのアドレスを取得
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	//頂点データをリソースにコピー
-	memcpy(vertexData, modelData.vertices.data(), sizeof(VertexData) * modelData.vertices.size());
-
-}
-
-void Object3D::CreateMaterialResource()
-{
-	// マテリアルリソースを作成
-	materialResource = object3DCommon->GetDxCommon()->CreateBufferResource(sizeof(Material));
-	// マテリアルリソースにデータを書き込むためのアドレスを取得してmaterialDataに割り当てる
-	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialData));
-	// マテリアルデータの初期化
-	materialData->color = { 1.0f, 1.0f, 1.0f, 1.0f };
-	materialData->enableLighting = 0;
-	materialData->uvTransform = MakeIdentity4x4();
 
 
-}
+
 
 void Object3D::CreateTransformationMatrixData()
 {
